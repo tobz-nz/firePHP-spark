19a284210e66668974dd40e30635a4127dea6807
Toby Evans <tobz.nz@gmail.com>
2011-08-29 08:46:49 +1200

    Add autoload
    Signed-off-by: Toby Evans <tobz.nz@gmail.com>

---
+++ config/autoload.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+# Load the autoform library when the spark is loaded
+$autoload['library'] = array('console');
+
+/* End of file autoload.php */
+/* Location: sparks/console/.../config/autoload.php */
