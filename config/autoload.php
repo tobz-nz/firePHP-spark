60ed61c211147a190c1096f4a1597ab2d804b728
Toby Evans <tobyevans@tobz.(none)>
2011-08-31 23:01:57 +1200

    fix autoloader

--- config/autoload.php
+++ config/autoload.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 # Load the autoform library when the spark is loaded
-$autoload['library'] = array('console');
+$autoload['libraries'] = array('console');
 
 /* End of file autoload.php */
 /* Location: sparks/console/.../config/autoload.php */
